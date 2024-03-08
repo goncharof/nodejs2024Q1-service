279c7d142e2eaebba0a191bc3ceda3975ab45ecd
yolo <igoncharov@flightpath3d.com>
2024-03-08 18:18:27 +0000

    feat: user.entity - exclude password

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,8 +1,10 @@
+import { Exclude } from 'class-transformer';
 import { UUID } from 'node:crypto';
 
 export class User {
   id: UUID;
   login: string;
+  @Exclude()
   password: string;
   version: number;
   createdAt: number;
