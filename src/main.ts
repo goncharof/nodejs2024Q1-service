1cbe5149f4921a074aa2141ca3577e5cf84da333
yolo <igoncharov@flightpath3d.com>
2024-03-11 16:06:36 +0000

    feat: swagger module setup

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,20 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule } from '@nestjs/swagger';
+import * as YAML from 'yamljs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  // const config = new DocumentBuilder()
+  //   .setTitle('Home Library Service')
+  //   .setDescription('Home music library service')
+  //   .setVersion('1.0')
+  //   .build();
+  // const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, YAML.load('./doc/api.yaml'));
 
   await app.listen(process.env.PORT || 4000);
 }
