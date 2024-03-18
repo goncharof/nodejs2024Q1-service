4965477a60ed5146ed73cae77a8a770bbd321223
yolo <igoncharov@flightpath3d.com>
2024-03-18 18:38:21 +0000

    feat: update user table

--- prisma/migrations/20240318183745_init/migration.sql
+++ prisma/migrations/20240318183745_init/migration.sql
@@ -2,7 +2,8 @@
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "login" TEXT NOT NULL,
-    "version" INTEGER NOT NULL,
+    "password" TEXT NOT NULL,
+    "version" INTEGER NOT NULL DEFAULT 1,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
